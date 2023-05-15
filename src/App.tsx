0dfb80316ae76bdaf2740e8e0879f8f8b4359506
Uristrix <mr.lux1@yandex.ru>
2023-05-16 02:59:20 +0300

    merge max_branch

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import MainPage from './pages/Mainpage/Mainpage';
 import ErrorPage from './pages/Errorpage/Errorpage';
 import Account from './pages/Account/Account';
 import SchedulePage from './pages/Schedule/SchedulePage';
-import { Test1, Test2, Test3, Test4 } from './pages/Tests';
+import { Test1, Test2, Test3, Test4, Test5, Test6, Test7, Test8 } from './pages/Tests';
 import {
   Topic4_1,
   Topic4_10,
@@ -46,6 +46,10 @@ const App = observer(() => {
             <Route path="/test2" element={<Test2 />} />
             <Route path="/test3" element={<Test3 />} />
             <Route path="/test4" element={<Test4 />} />
+            <Route path="/test5" element={<Test5 />} />
+            <Route path="/test6" element={<Test6 />} />
+            <Route path="/test7" element={<Test7 />} />
+            <Route path="/test8" element={<Test8 />} />
             <Route path="/lecture">
               <Route path="/lecture/4_1" element={<Topic4_1 />} />
               <Route path="/lecture/4_2" element={<Topic4_2 />} />
---
+++ src/components/Exercise/components/Task6_1.tsx
@@ -0,0 +1,57 @@
+import Input from '../../Input/Input';
+import { useState } from 'react';
+import classNames from 'classnames';
+import InputBlock2 from './InputBlock2';
+import Button from '../../Button/Button';
+
+export const Task6_1 = (props) => {
+  const [inputCount, setInputCount] = useState(0);
+  return (
+    <div className="m-auto">
+      <div className="flex mb-2">
+        [X]п = -9/16 =
+        <div className="w-60">
+          <Input classes={{ input: 'ml-[5px]' }} />
+        </div>
+      </div>
+      <div className="flex">
+        [Y]п = -9/16 =
+        <div className="w-60">
+          <Input classes={{ input: 'ml-[5px]' }} />
+        </div>
+      </div>
+      <InputBlock2 />
+      {[...Array(inputCount)].map((inputCount) => (
+        <InputBlock2 key={inputCount} />
+      ))}
+      <div className={classNames('flex min-w-[40px] md:min-w-[58px] m-auto mb-2')}>
+        <div className="flex w-[150px] ml-[90px]">
+          <Input classes={{ input: '!rounded-none' }} />
+        </div>
+        <div className="flex w-[100px] ml-2">
+          <Input classes={{ input: '!rounded-none' }} />
+        </div>
+      </div>
+      <Button
+        onClick={() => {
+          setInputCount(inputCount + 1);
+        }}
+      >
+        Добавить блок
+      </Button>
+      <Button
+        onClick={() => {
+          if (inputCount > 0) setInputCount(inputCount - 1);
+        }}
+      >
+        Убрать блок
+      </Button>
+      <div className="flex mt-2">
+        Ответ =
+        <div className="w-60">
+          <Input classes={{ input: 'ml-[5px]' }} />
+        </div>
+      </div>
+    </div>
+  );
+};
