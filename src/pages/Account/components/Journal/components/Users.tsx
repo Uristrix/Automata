71a6ba19c387a30302ef56738ca22ae498aff206
ammylt <80398061+ammylt@users.noreply.github.com>
2023-05-22 16:42:29 +0300

    add butExcel

--- src/pages/Account/components/Journal/components/Users.tsx
+++ src/pages/Account/components/Journal/components/Users.tsx
@@ -6,13 +6,28 @@ import notification from '../../../../../store/notification';
 import { Group } from '../../../../../model/group';
 import { Inputs } from '../../../../../model/Inputs';
 import { postUser } from '../../../../../utils/user';
-
+import { read, utils } from 'xlsx';
 const users = 'user';
 
 const AddUser = ({ groups, trigger }: { groups: Array<Group>; trigger: () => void }) => {
   const [InputGroup, setInputGroup] = useState<{ value: string; label: string } | null>(null);
   const [inputUser, setInputUser] = useState<Inputs>({});
   const generateOption = groups.map((el) => ({ value: `${el.id}` || '', label: el.name || '' }));
+
+  const handleImport = ($event) => {
+    const files = $event.target.files;
+    if (files.length) {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const wb = read(event.target!.result);
+        const sheets = wb.SheetNames;
+        const result = utils.sheet_to_json(wb.Sheets[sheets[0]]);
+        console.log(result);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  };
 
   const handlerSubmit = async (e) => {
     e.preventDefault();
@@ -31,7 +46,7 @@ const AddUser = ({ groups, trigger }: { groups: Array<Group>; trigger: () => voi
   };
 
   return (
-    <form className="h-[400px] w-[300px] md:w-[650px] md:w-full p-3" onSubmit={handlerSubmit}>
+    <form className="h-[420px] w-[300px] md:w-[650px] md:w-full p-3" onSubmit={handlerSubmit}>
       <h2 className="w-full text-center text-xl font-semibold mb-5">Управление пользователями</h2>
       <div className="w-full flex flex-col items-center justify-between gap-4 mb-5">
         <Input
@@ -50,7 +65,16 @@ const AddUser = ({ groups, trigger }: { groups: Array<Group>; trigger: () => voi
           value={inputUser[users]?.password || ''}
           onChange={(e) => setInputUser({ ...inputUser, [users]: { ...inputUser[users], password: e.target.value } })}
         />
-        <Button style="w-full md:w-[300px] mt-5" type="submit">
+        <Input
+          type="file"
+          name="file"
+          className="custom-file-input"
+          id="inputGroupFile"
+          required
+          onChange={handleImport}
+          accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+        />
+        <Button style="w-full md:w-[300px] mt-1" type="submit">
           Добавить
         </Button>
       </div>
